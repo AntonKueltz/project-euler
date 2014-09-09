d9349e638c12d68d3597c8ee57f4505744f61988
Anton Kueltz <kueltz.anton@gmail.com>
2014-09-09 19:18:01 -0400

    479 added, solution verified

--- 100+/euler479.py
+++ 100+/euler479.py
@@ -1,14 +1,45 @@
-import numpy
+m = 1000000007
 
-def euler479():
-    # 1/x = (k/x)^2 * (k+x^2) - kx
-    # 1/x = k^2/x^2 * (k+x^2) - kx
-    # 1/x = (k^3 + k^2x^2) / x^2 -kx
-    # 1/x = k^3/x^2 + k^2 - kx
-    # 1 = k^3 / x + k^2x - kx^2
-    # 0 = k^3 / x + k^2x - kx^2 - 1
-    # 0 = k^3 - x + k^2x^2 - kx^3
-    pass
+# modular exponentiation
+def modexp(base, exp, mod):
+	neg = base < 0
+	base = base % mod
+	res = 1
+	
+	while exp > 0:
+		if exp % 2 == 1:
+			res = (res * base) % mod
+		exp = exp >> 1
+		base = (base * base) % mod
+	
+	if neg: return res - mod
+	else: return res
+
+# extended gcd	
+def egcd(a, b):
+	if a == 0: return (b, 0, 1)
+	else:
+		g, y, x = egcd(b % a, a)
+		return (g, x-(b // a)*y, y)
+
+# modular inverse using extended gcd
+def modinv(a, mod):
+	gcd, x, y = egcd(a, mod)
+	if gcd != 1: return None
+	else: return x % mod
+	
+def euler479(*args):
+	base, gap = 0, -3
+	n, s = 10**6, 0
+	
+	for i in range(1, n+1):
+		tmp = (base - modexp(base, n+1, m)) *  modinv(1-base, m)
+		s = (s + tmp) % m
+		
+		base += gap
+		gap += -2
+    	
+	return s % m
 
 if __name__ == "__main__":
-    print euler479()
+	print euler479()
