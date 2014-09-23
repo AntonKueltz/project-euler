24c987bc0111a6404efecdae2b722c47ac0339b7
Anton Kueltz <kueltz.anton@gmail.com>
2014-09-22 23:58:12 -0400

    Un-hardcoded problem 5

--- euler05.py
+++ euler05.py
@@ -1,13 +1,19 @@
-def euler05():
-	"""
-	2520 is the smallest number that can be divided by each of the numbers
-	from 1 to 10 without any remainder.
+from fractions import gcd
 
-	What is the smallest positive number that is evenly divisible by all of
-	the numbers from 1 to 20?
-	"""
-	# if a number is not prime only multiply by the factors not already present
-	return 1*2*3*2*5*1*7*2*3*1*11*1*13*1*1*2*17*1*19*1
-	
+def euler05():
+        """
+        2520 is the smallest number that can be divided by each of the numbers
+        from 1 to 10 without any remainder.
+
+        What is the smallest positive number that is evenly divisible by all of
+        the numbers from 1 to 20?
+        """
+        num = 1
+        
+        for mult in range(1, 21):
+                num *= mult / gcd(num, mult)
+
+        return num
+        
 if __name__ == "__main__":
-	print euler05()
+        print euler05()
